5123c4b169faf567c79de058771c5223a9f12739
Vaiibhavi_Singh <vaiibhavisingh855@gmail.com>
2026-09-20 20:39:17 +0530

    Add Week 4 java program

---
+++ MembershipCardMain.java
@@ -0,0 +1,41 @@
+class MembershipCard {
+
+    static String libraryName;
+    static String validUntil;
+
+    String studentName;
+
+    static {
+        libraryName = "SRM Central Library";
+        validUntil = "May 2027";
+
+        System.out.println("Library info loaded");
+    }
+
+    MembershipCard(String studentName) {
+        this.studentName = studentName;
+    }
+}
+
+public class MembershipCardMain {
+    public static void main(String[] args) {
+
+        String[] names = {
+            "Ananya",
+            "Rohan",
+            "Priya",
+            "Arjun",
+            "Sneha"
+        };
+
+        for (int i = 0; i < names.length; i++) {
+
+            MembershipCard card =
+                new MembershipCard(names[i]);
+
+            System.out.println(
+                "Membership card issued: " + card.studentName
+            );
+        }
+    }
+}
